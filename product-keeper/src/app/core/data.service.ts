19df8b2e990570735de754057b55e19b676356a6
ryan-mllrdev <ryan.repe@mllrdev.com>
2020-06-17 22:12:05 +0800

    Updated data service to set the foreign name of each item in products and suppliers list, e.g. supplierName and addressName.

--- product-keeper/src/app/core/data.service.ts
+++ product-keeper/src/app/core/data.service.ts
@@ -4,7 +4,7 @@ import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { map, catchError } from 'rxjs/operators';
 
-import { IProduct } from '../shared/interfaces';
+import { IProduct, ISupplier, IAddress } from '../shared/interfaces';
 
 @Injectable()
 export class DataService {
@@ -16,6 +16,53 @@ export class DataService {
     getProducts() : Observable<IProduct[]> {
         return this.http.get<IProduct[]>(this.baseUrl + 'products.json').
         pipe(
+            map(products => {
+                products.forEach(element => {
+
+                    this.getSupplierName(element.supplierId)
+                    .subscribe((name: string) => element.supplierName = name);
+
+                });
+                return products;
+            }),
+            catchError(this.handleError)
+        );
+    }
+
+    getSuppliers() : Observable<ISupplier[]> {
+        return this.http.get<ISupplier[]>(this.baseUrl + 'suppliers.json').
+        pipe(
+            map(suppliers => {
+                suppliers.forEach(element => {
+
+                    this.getAddressName(element.addressId)
+                    .subscribe((name: string) => element.addressName = name);
+
+                });
+                return suppliers;
+            }),
+            catchError(this.handleError)
+        );
+    }
+
+    getSupplierName(id: number): Observable<string> {
+        return this.http.get<ISupplier[]>(this.baseUrl + 'suppliers.json')
+        .pipe(
+            map(suppliers => {
+                let supplier = suppliers.filter((sup: ISupplier) => sup.id === id);
+                return (supplier && supplier.length) ? supplier[0].name : null;
+            }),
+            catchError(this.handleError)
+        );
+    }
+
+    getAddressName(id: number): Observable<string> {
+        return this.http.get<IAddress[]>(this.baseUrl + 'locations.json')
+        .pipe(
+            map(locations => {
+                let location = locations.filter((loc: IAddress) => loc.id === id);
+                return (location && location.length) ? location[0].name : null;
+            }),
             catchError(this.handleError)
         );
     }
