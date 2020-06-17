a7e4cb4d2acbbdf0adf7391cb523b5cbaa887502
ryan-mllrdev <ryan.repe@mllrdev.com>
2020-06-17 22:10:34 +0800

    Added new property to display foreign  names.

--- product-keeper/src/app/shared/interfaces.ts
+++ product-keeper/src/app/shared/interfaces.ts
@@ -1,15 +1,17 @@
+export interface ISupplier {
+    id: number;
+    name: string;
+    addressId: number;
+    addressName: string;
+}
+
 export interface IProduct {
     id: number;
     sku: string;
     name: string;
     description: string;
     supplierId: number;
-}
-
-export interface ISupplier {
-    id: number;
-    name: string;
-    addressId: number;
+    supplierName: string;
 }
 
 export interface IAddress {
