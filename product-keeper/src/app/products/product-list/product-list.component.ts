398f09f6b407097e97aa7987be8f6f866b31babf
ryan-mllrdev <ryan.repe@mllrdev.com>
2020-06-17 21:16:13 +0800

    Bind product values to the UI.

--- product-keeper/src/app/products/product-list/product-list.component.ts
+++ product-keeper/src/app/products/product-list/product-list.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { IProduct } from 'src/app/shared/interfaces';
 
 @Component({
   selector: 'app-product-list',
@@ -7,9 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductListComponent implements OnInit {
 
+  filteredProducts: IProduct[] = [];
+
   constructor() { }
 
   ngOnInit(): void {
   }
+
+  @Input () products: IProduct[];
 
 }
