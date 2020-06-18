86e2854f77871adc73683db7ed37572e77c6074f
ryan-mllrdev <ryan.repe@mllrdev.com>
2020-06-18 19:13:36 +0800

    Added add/edit folder for locations and suppliers.

--- product-keeper/src/app/admin/locations/locations.module.ts
+++ product-keeper/src/app/admin/locations/locations.module.ts
@@ -7,10 +7,11 @@ import { LocationsComponent } from './locations.component';
 import { FormsModule } from '@angular/forms';
 import { Ng2SearchPipeModule } from 'ng2-search-filter';
 import { RouterModule } from '@angular/router';
+import { AddEditComponent } from './add-edit/add-edit.component';
 
 @NgModule({
     imports: [CommonModule,RouterModule,FormsModule,Ng2SearchPipeModule],
-    declarations: [LocationsComponent],
+    declarations: [LocationsComponent, AddEditComponent],
     exports: [LocationsComponent]
 })
 export class LocationsModule {}
