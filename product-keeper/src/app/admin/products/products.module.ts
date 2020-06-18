52fc99cee9c53da1b5fc61a648d32265b00e108e
ryan-mllrdev <ryan.repe@mllrdev.com>
2020-06-18 14:15:27 +0800

    Added the FormsModule to make ngModel directive work.

--- product-keeper/src/app/admin/products/products.module.ts
+++ product-keeper/src/app/admin/products/products.module.ts
@@ -6,9 +6,10 @@ import { ProductListComponent } from './product-list/product-list.component';
 import { ProductAddEditComponent } from './add-edit/add-edit.component';
 import { DetailsComponent } from './details/details.component';
 import { RouterModule } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
-    imports: [CommonModule,RouterModule],
+    imports: [CommonModule,RouterModule,FormsModule],
     declarations: [ProductsComponent, ProductListComponent, ProductAddEditComponent, DetailsComponent],
     exports: [ProductsComponent]
 })
